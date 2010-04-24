mola/qgis

commit 69c3a91fa8a861774c97a2f70f9b35e5ad34d0b6

spatial query plugin: fix warnings, reindent, include in debian package
git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13360 c8812cc2-4d05-0410-92ff-de0c093fc19c

src/plugins/spatialquery/qgsspatialquery.cpp
@@ -25,163 +25,166 @@
 #include "qgsgeometrycoordinatetransform.h"
 #include "qgsspatialquery.h"
 
-QgsSpatialQuery::QgsSpatialQuery(MngProgressBar *pb)
+QgsSpatialQuery::QgsSpatialQuery( MngProgressBar *pb )
 {
-    mPb = pb;
-    mUseTargetSelection = mUseReferenceSelection = false;
+  mPb = pb;
+  mUseTargetSelection = mUseReferenceSelection = false;
 
 } // QgsSpatialQuery::QgsSpatialQuery(MngProgressBar *pb)
 
 QgsSpatialQuery::~QgsSpatialQuery()
 {
-    delete mReaderFeaturesTarget;
+  delete mReaderFeaturesTarget;
 
 } // QgsSpatialQuery::~QgsSpatialQuery()
 
-void QgsSpatialQuery::setSelectedFeaturesTarget(bool useSelected)
+void QgsSpatialQuery::setSelectedFeaturesTarget( bool useSelected )
 {
-    mUseTargetSelection = useSelected;
+  mUseTargetSelection = useSelected;
 
 } // void QgsSpatialQuery::setSelectedFeaturesTarget(bool useSelected)
 
-void QgsSpatialQuery::setSelectedFeaturesReference(bool useSelected)
+void QgsSpatialQuery::setSelectedFeaturesReference( bool useSelected )
 {
-    mUseReferenceSelection = useSelected;
+  mUseReferenceSelection = useSelected;
 
 } // void QgsSpatialQuery::setSelectedFeaturesReference(bool useSelected)
 
 void QgsSpatialQuery::runQuery( QSet<int> & qsetIndexResult, int relation, QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference )
 {
-    setQuery(lyrTarget, lyrReference);
+  setQuery( lyrTarget, lyrReference );
 
-    // Create Spatial index for Reference - Set mIndexReference
-    mPb->setFormat("Processing 1/2 - %p%");
-    int totalStep = mUseReferenceSelection
-                    ? mLayerReference->selectedFeatureCount()
-                    : (int) (mLayerReference->featureCount());
-    mPb->init(1, totalStep);
-    setSpatialIndexReference(); // Need set mLayerReference before
+  // Create Spatial index for Reference - Set mIndexReference
+  mPb->setFormat( "Processing 1/2 - %p%" );
+  int totalStep = mUseReferenceSelection
+                  ? mLayerReference->selectedFeatureCount()
+                  : ( int )( mLayerReference->featureCount() );
+  mPb->init( 1, totalStep );
+  setSpatialIndexReference(); // Need set mLayerReference before
 
-    // Make Query
-    mPb->setFormat("Processing 2/2 - %p%");
-    totalStep = mUseTargetSelection
-                ? mLayerTarget->selectedFeatureCount()
-                : (int) (mLayerTarget->featureCount());
-    mPb->init(1, totalStep);
+  // Make Query
+  mPb->setFormat( "Processing 2/2 - %p%" );
+  totalStep = mUseTargetSelection
+              ? mLayerTarget->selectedFeatureCount()
+              : ( int )( mLayerTarget->featureCount() );
+  mPb->init( 1, totalStep );
 
-    execQuery(qsetIndexResult, relation);
+  execQuery( qsetIndexResult, relation );
 
 } // QSet<int> QgsSpatialQuery::runQuery( int relation)
 
-QMap<QString, int>* QgsSpatialQuery::getTypesOperations(QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference)
+QMap<QString, int>* QgsSpatialQuery::getTypesOperations( QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference )
 {
-    /* Relations from OGC document (obtain in February 2010)
-       06-103r3_Candidate_Implementation_Specification_for_Geographic_Information_-_Simple_feature_access_-_Part_1_Common_Architecture_v1.2.0.pdf
+  /* Relations from OGC document (obtain in February 2010)
+     06-103r3_Candidate_Implementation_Specification_for_Geographic_Information_-_Simple_feature_access_-_Part_1_Common_Architecture_v1.2.0.pdf
 
-       (P)oint, (L)ine and (A)rea
-       Target Geometry(P,L,A) / Reference Geometry (P,L,A)
-       dim -> Dimension of geometry
-       Relations:
-        1) Intersects and Disjoint: All
-        2) Touches: P/L P/A L/L L/A A/A
-           dimReference  > dimTarget OR dimReference = dimTarget if dimReference > 0
-        3) Crosses: P/L P/A L/L L/A
-           dimReference  > dimTarget OR dimReference = dimTarget if dimReference = 1
-        4) Within: P/L P/A L/A A/A
-           dimReference  > dimTarget OR dimReference = dimTarget if dimReference = 2
-        5) Equals: P/P L/L A/A
-           dimReference = dimTarget
-        6) Overlaps: P/P L/L A/A
-           dimReference = dimTarget
-        7) Contains: L/P A/P A/L A/A
-           dimReference  <  dimTarget OR dimReference = dimTarget if dimReference = 2
-    */
+     (P)oint, (L)ine and (A)rea
+     Target Geometry(P,L,A) / Reference Geometry (P,L,A)
+     dim -> Dimension of geometry
+     Relations:
+      1) Intersects and Disjoint: All
+      2) Touches: P/L P/A L/L L/A A/A
+         dimReference  > dimTarget OR dimReference = dimTarget if dimReference > 0
+      3) Crosses: P/L P/A L/L L/A
+         dimReference  > dimTarget OR dimReference = dimTarget if dimReference = 1
+      4) Within: P/L P/A L/A A/A
+         dimReference  > dimTarget OR dimReference = dimTarget if dimReference = 2
+      5) Equals: P/P L/L A/A
+         dimReference = dimTarget
+      6) Overlaps: P/P L/L A/A
+         dimReference = dimTarget
+      7) Contains: L/P A/P A/L A/A
+         dimReference  <  dimTarget OR dimReference = dimTarget if dimReference = 2
+  */
 
-    QMap<QString, int> * operations = new QMap<QString, int>;
-    operations->insert(QObject::tr("Intersects"), Intersects);
-    operations->insert(QObject::tr("Disjoint"), Disjoint);
+  QMap<QString, int> * operations = new QMap<QString, int>;
+  operations->insert( QObject::tr( "Intersects" ), Intersects );
+  operations->insert( QObject::tr( "Disjoint" ), Disjoint );
 
-    short int dimTarget=0, dimReference=0;
-    dimTarget = dimensionGeometry(lyrTarget->geometryType());
-    dimReference = dimensionGeometry(lyrReference->geometryType());
+  short int dimTarget = 0, dimReference = 0;
+  dimTarget = dimensionGeometry( lyrTarget->geometryType() );
+  dimReference = dimensionGeometry( lyrReference->geometryType() );
 
-    if (dimReference > dimTarget)
+  if ( dimReference > dimTarget )
+  {
+    operations->insert( QObject::tr( "Touches" ), Touches );
+    operations->insert( QObject::tr( "Crosses" ), Crosses );
+    operations->insert( QObject::tr( "Within" ), Within );
+  }
+  else if ( dimReference < dimTarget )
+  {
+    operations->insert( QObject::tr( "Contains" ), Contains );
+  }
+  else // dimReference == dimTarget
+  {
+    operations->insert( QObject::tr( "Equals" ), Equals );
+    operations->insert( QObject::tr( "Overlaps" ), Overlaps );
+    switch ( dimReference )
     {
-        operations->insert(QObject::tr("Touches"), Touches);
-        operations->insert(QObject::tr("Crosses"), Crosses);
-        operations->insert(QObject::tr("Within"), Within);
+      case 0:
+        break;
+      case 1:
+        operations->insert( QObject::tr( "Touches" ), Touches );
+        operations->insert( QObject::tr( "Crosses" ), Crosses );
+        break;
+      case 2:
+        operations->insert( QObject::tr( "Touches" ), Touches );
+        operations->insert( QObject::tr( "Within" ), Within );
+        operations->insert( QObject::tr( "Contains" ), Contains );
     }
-    else if (dimReference < dimTarget)
-    {
-        operations->insert(QObject::tr("Contains"), Contains);
-    }
-    else // dimReference == dimTarget
-    {
-        operations->insert(QObject::tr("Equals"), Equals);
-        operations->insert(QObject::tr("Overlaps"), Overlaps);
-        switch (dimReference)
-        {
-        case 0:
-            break;
-        case 1:
-            operations->insert(QObject::tr("Touches"), Touches);
-            operations->insert(QObject::tr("Crosses"), Crosses);
-            break;
-        case 2:
-            operations->insert(QObject::tr("Touches"), Touches);
-            operations->insert(QObject::tr("Within"), Within);
-            operations->insert(QObject::tr("Contains"), Contains);
-        }
-    }
-    return operations;
+  }
+  return operations;
 
 } // QMap<QString, int>* QgsSpatialQuery::getTypesOperators(QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference)
 
-short int QgsSpatialQuery::dimensionGeometry(QGis::GeometryType geomType)
+short int QgsSpatialQuery::dimensionGeometry( QGis::GeometryType geomType )
 {
-    int dimGeom = 0;
-    switch (geomType)
-    {
+  int dimGeom = 0;
+  switch ( geomType )
+  {
     case QGis::Point:
-        dimGeom = 0;
-        break;
+      dimGeom = 0;
+      break;
     case QGis::Line:
-        dimGeom = 1;
-        break;
+      dimGeom = 1;
+      break;
     case QGis::Polygon:
-        dimGeom = 2;
-    }
-    return dimGeom;
+      dimGeom = 2;
+    default:
+      Q_ASSERT( 0 );
+      dimGeom = 0;
+  }
+  return dimGeom;
 
 } // int QgsSpatialQuery::dimensionGeometry(QGis::GeometryType geomType)
 
-void QgsSpatialQuery::setQuery (QgsVectorLayer *layerTarget, QgsVectorLayer *layerReference)
+void QgsSpatialQuery::setQuery( QgsVectorLayer *layerTarget, QgsVectorLayer *layerReference )
 {
-    mLayerTarget = layerTarget;
-    mReaderFeaturesTarget = new QgsReaderFeatures(mLayerTarget, mUseTargetSelection);
-    mLayerReference = layerReference;
+  mLayerTarget = layerTarget;
+  mReaderFeaturesTarget = new QgsReaderFeatures( mLayerTarget, mUseTargetSelection );
+  mLayerReference = layerReference;
 
 } // void QgsSpatialQuery::setQuery (QgsVectorLayer *layerTarget, QgsVectorLayer *layerReference)
 
-bool QgsSpatialQuery::hasValidGeometry(QgsFeature &feature)
+bool QgsSpatialQuery::hasValidGeometry( QgsFeature &feature )
 {
   if ( ! feature.isValid() )
   {
-      return false;
+    return false;
   }
 
   QgsGeometry *geom = feature.geometry();
 
   if ( NULL == geom )
   {
-      return false;
+    return false;
   }
 
   GEOSGeometry *geomGeos = geom->asGeos();
 
-  if ( GEOSisEmpty(geomGeos) || 1 !=  GEOSisValid(geomGeos))
+  if ( GEOSisEmpty( geomGeos ) || 1 !=  GEOSisValid( geomGeos ) )
   {
-      return false;
+    return false;
   }
 
   return true;
@@ -190,153 +193,153 @@ bool QgsSpatialQuery::hasValidGeometry(QgsFeature &feature)
 
 void QgsSpatialQuery::setSpatialIndexReference()
 {
-    QgsReaderFeatures * readerFeaturesReference = new QgsReaderFeatures(mLayerReference, mUseReferenceSelection);
-    QgsFeature feature;
-    int step = 1;
-    while ( true )
+  QgsReaderFeatures * readerFeaturesReference = new QgsReaderFeatures( mLayerReference, mUseReferenceSelection );
+  QgsFeature feature;
+  int step = 1;
+  while ( true )
+  {
+    if ( ! readerFeaturesReference->nextFeature( feature ) )
     {
-        if ( ! readerFeaturesReference->nextFeature(feature) )
-        {
-            break;
-        }
-        mPb->step(step++);
-
-        if ( ! hasValidGeometry(feature) )
-        {
-            continue;
-        }
-
-        mIndexReference.insertFeature(feature);
+      break;
     }
-    delete readerFeaturesReference;
+    mPb->step( step++ );
+
+    if ( ! hasValidGeometry( feature ) )
+    {
+      continue;
+    }
+
+    mIndexReference.insertFeature( feature );
+  }
+  delete readerFeaturesReference;
 
 } // void QgsSpatialQuery::setSpatialIndexReference()
 
-void QgsSpatialQuery::execQuery( QSet<int> & qsetIndexResult, int relation)
+void QgsSpatialQuery::execQuery( QSet<int> & qsetIndexResult, int relation )
 {
-    // Set function GEOS
-    char(*operation)(const GEOSGeometry *, const GEOSGeometry*);
-    switch (relation)
-    {
+  // Set function GEOS
+  char( *operation )( const GEOSGeometry *, const GEOSGeometry* );
+  switch ( relation )
+  {
     case Disjoint:
-        operation = &GEOSDisjoint;
-        break;
+      operation = &GEOSDisjoint;
+      break;
     case Equals:
-        operation = &GEOSEquals;
-        break;
+      operation = &GEOSEquals;
+      break;
     case Touches:
-        operation = &GEOSTouches;
-        break;
+      operation = &GEOSTouches;
+      break;
     case Overlaps:
-        operation = &GEOSOverlaps;
-        break;
+      operation = &GEOSOverlaps;
+      break;
     case Within:
-        operation = &GEOSWithin;
-        break;
+      operation = &GEOSWithin;
+      break;
     case Contains:
-        operation = &GEOSContains;
-        break;
+      operation = &GEOSContains;
+      break;
     case Crosses:
-        operation = &GEOSCrosses;
-        break;
+      operation = &GEOSCrosses;
+      break;
     case Intersects:
-        operation = &GEOSIntersects;
-        break;
-    }
+      operation = &GEOSIntersects;
+      break;
+  }
 
-    // Transform referencer Target = Reference
-    QgsGeometryCoordinateTransform *coordinateTransform = new QgsGeometryCoordinateTransform();
-    coordinateTransform->setCoordinateTransform(mLayerTarget, mLayerReference);
+  // Transform referencer Target = Reference
+  QgsGeometryCoordinateTransform *coordinateTransform = new QgsGeometryCoordinateTransform();
+  coordinateTransform->setCoordinateTransform( mLayerTarget, mLayerReference );
 
-    // Set function for populate result
-    void (QgsSpatialQuery::* funcPopulateIndexResult)
-            (QSet<int> &, int, QgsGeometry *,
-             char(*)(const GEOSGeometry *, const GEOSGeometry *) );
-    funcPopulateIndexResult = ( relation == Disjoint )
-                              ? &QgsSpatialQuery::populateIndexResultDisjoint
-                              : &QgsSpatialQuery::populateIndexResult;
+  // Set function for populate result
+  void ( QgsSpatialQuery::* funcPopulateIndexResult )
+  ( QSet<int> &, int, QgsGeometry *,
+    char( * )( const GEOSGeometry *, const GEOSGeometry * ) );
+  funcPopulateIndexResult = ( relation == Disjoint )
+                            ? &QgsSpatialQuery::populateIndexResultDisjoint
+                            : &QgsSpatialQuery::populateIndexResult;
 
-    QgsFeature featureTarget;
-    QgsGeometry * geomTarget;
-    int step = 1;
-    while( true )
+  QgsFeature featureTarget;
+  QgsGeometry * geomTarget;
+  int step = 1;
+  while ( true )
+  {
+    if ( ! mReaderFeaturesTarget->nextFeature( featureTarget ) ) break;
+
+    mPb->step( step++ );
+
+    if ( ! hasValidGeometry( featureTarget ) )
     {
-        if ( ! mReaderFeaturesTarget->nextFeature(featureTarget) ) break;
-
-        mPb->step(step++);
-
-        if ( ! hasValidGeometry(featureTarget) )
-        {
-            continue;
-        }
-
-        geomTarget = featureTarget.geometry();
-        coordinateTransform->transform(geomTarget);
-
-        (this->*funcPopulateIndexResult)
-              (qsetIndexResult, featureTarget.id(), geomTarget, operation);
+      continue;
     }
-    delete coordinateTransform;
+
+    geomTarget = featureTarget.geometry();
+    coordinateTransform->transform( geomTarget );
+
+    ( this->*funcPopulateIndexResult )
+    ( qsetIndexResult, featureTarget.id(), geomTarget, operation );
+  }
+  delete coordinateTransform;
 
 } // QSet<int> QgsSpatialQuery::execQuery( QSet<int> & qsetIndexResult, int relation)
 
 void QgsSpatialQuery::populateIndexResult(
-        QSet<int> &qsetIndexResult, int idTarget, QgsGeometry * geomTarget,
-        char(*operation)(const GEOSGeometry *, const GEOSGeometry *) )
+  QSet<int> &qsetIndexResult, int idTarget, QgsGeometry * geomTarget,
+  char( *operation )( const GEOSGeometry *, const GEOSGeometry * ) )
 {
-    QList<int> listIdReference;
-    listIdReference = mIndexReference.intersects(geomTarget->boundingBox());
-    if ( listIdReference.count() == 0 )
+  QList<int> listIdReference;
+  listIdReference = mIndexReference.intersects( geomTarget->boundingBox() );
+  if ( listIdReference.count() == 0 )
+  {
+    return;
+  }
+  GEOSGeometry * geosTarget = geomTarget->asGeos();
+  QgsFeature featureReference;
+  QgsGeometry * geomReference;
+  QList<int>::iterator iterIdReference = listIdReference.begin();
+  for ( ; iterIdReference != listIdReference.end(); iterIdReference++ )
+  {
+    mLayerReference->featureAtId( *iterIdReference, featureReference );
+    geomReference = featureReference.geometry();
+    if (( *operation )( geosTarget, geomReference->asGeos() ) == 1 )
     {
-        return;
+      qsetIndexResult.insert( idTarget );
+      break;
     }
-    GEOSGeometry * geosTarget = geomTarget->asGeos();
-    QgsFeature featureReference;
-    QgsGeometry * geomReference;
-    QList<int>::iterator iterIdReference = listIdReference.begin();
-    for( ; iterIdReference != listIdReference.end(); iterIdReference++ )
-    {
-        mLayerReference->featureAtId(*iterIdReference, featureReference);
-        geomReference = featureReference.geometry();
-        if( (*operation)(geosTarget, geomReference->asGeos()) == 1 )
-        {
-            qsetIndexResult.insert(idTarget);
-            break;
-        }
-    }
+  }
 
 } // void QgsSpatialQuery::populateIndexResult(...
 
 void QgsSpatialQuery::populateIndexResultDisjoint(
-        QSet<int> &qsetIndexResult, int idTarget, QgsGeometry * geomTarget,
-        char(*operation)(const GEOSGeometry *, const GEOSGeometry *) )
+  QSet<int> &qsetIndexResult, int idTarget, QgsGeometry * geomTarget,
+  char( *operation )( const GEOSGeometry *, const GEOSGeometry * ) )
 {
-    QList<int> listIdReference;
-    listIdReference = mIndexReference.intersects(geomTarget->boundingBox());
-    if ( listIdReference.count() == 0 )
+  QList<int> listIdReference;
+  listIdReference = mIndexReference.intersects( geomTarget->boundingBox() );
+  if ( listIdReference.count() == 0 )
+  {
+    qsetIndexResult.insert( idTarget );
+    return;
+  }
+  GEOSGeometry * geosTarget = geomTarget->asGeos();
+  QgsFeature featureReference;
+  QgsGeometry * geomReference;
+  QList<int>::iterator iterIdReference = listIdReference.begin();
+  bool addIndex = true;
+  for ( ; iterIdReference != listIdReference.end(); iterIdReference++ )
+  {
+    mLayerReference->featureAtId( *iterIdReference, featureReference );
+    geomReference = featureReference.geometry();
+    if (( *operation )( geosTarget, geomReference->asGeos() ) == 0 )
     {
-        qsetIndexResult.insert(idTarget);
-        return;
+      addIndex = false;
+      break;
     }
-    GEOSGeometry * geosTarget = geomTarget->asGeos();
-    QgsFeature featureReference;
-    QgsGeometry * geomReference;
-    QList<int>::iterator iterIdReference = listIdReference.begin();
-    bool addIndex = true;
-    for( ; iterIdReference != listIdReference.end(); iterIdReference++ )
-    {
-        mLayerReference->featureAtId(*iterIdReference, featureReference);
-        geomReference = featureReference.geometry();
-        if( (*operation)(geosTarget, geomReference->asGeos()) == 0 )
-        {
-            addIndex = false;
-            break;
-        }
-    }
-    if ( addIndex )
-    {
-        qsetIndexResult.insert(idTarget);
-    }
+  }
+  if ( addIndex )
+  {
+    qsetIndexResult.insert( idTarget );
+  }
 
 } // void QgsSpatialQuery::populateIndexResultDisjoint( ...
 

debian/qgis.install
@@ -26,6 +26,7 @@ usr/lib/qgis/libevis.so
 usr/lib/qgis/libosmprovider.so 
 usr/lib/qgis/librasterterrainplugin.so
 usr/lib/qgis/liblabelingplugin.so
+usr/lib/qgis/libspatialqueryplugin.so
 usr/share/pixmaps/qgis-icon.xpm
 usr/share/pixmaps/qgis-mime-icon.png
 usr/share/pixmaps/qgis-mime-icon.png usr/share/icons/crystalsvg/128x128/mimetypes

debian/rules
@@ -10,7 +10,7 @@ DEB_HOST_GNU_TYPE   ?= $(shell dpkg-architecture -qDEB_HOST_GNU_TYPE)
 DEB_BUILD_GNU_TYPE  ?= $(shell dpkg-architecture -qDEB_BUILD_GNU_TYPE)
 
 DISTRIBUTION := $(shell dpkg-parsechangelog --format rfc822 | sed -ne "s/^Distribution: //p")
-ifneq ($(DISTRIBUTION),$(findstring $(DISTRIBUTION),"lenny hardy intrepid jaunty karmic"))
+ifneq ($(DISTRIBUTION),$(findstring $(DISTRIBUTION),"lenny hardy intrepid jaunty karmic lucid"))
 DISTRIBUTION := sid
 endif
 

src/plugins/spatialquery/CMakeLists.txt
@@ -14,11 +14,6 @@ SET (SPATIALQUERY_SRCS
 SET (SPATIALQUERY_MOC_HDRS 
      qgsspatialqueryplugin.h
      qgsspatialquerydialog.h
-     qgsspatialquery.h
-     qgsreaderfeatures.h
-     qgsrubberselectid.h
-     qgsgeometrycoordinatetransform.h
-     qgsmngprogressbar.h
 )
 
 SET( SPATIALQUERY_UIS qgsspatialquerydialogbase.ui)

src/plugins/spatialquery/qgsgeometrycoordinatetransform.cpp
@@ -23,33 +23,33 @@
 
 QgsGeometryCoordinateTransform::~QgsGeometryCoordinateTransform()
 {
-    delete mCoordTransform;
+  delete mCoordTransform;
 
 } // QgsGeometryCoordinateTransform::~QgsGeometryCoordinateTransform()
 
-void QgsGeometryCoordinateTransform::setCoordinateTransform(QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference)
+void QgsGeometryCoordinateTransform::setCoordinateTransform( QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference )
 {
-    // Transform Forward: Target to Reference
-    // * Use srs() to use old versions QGis - will be deprecited in 2.0 (after use crs())
-    QgsCoordinateReferenceSystem srsTarget = lyrTarget->srs();
-    QgsCoordinateReferenceSystem srsReference = lyrReference->srs();
+  // Transform Forward: Target to Reference
+  // * Use srs() to use old versions QGis - will be deprecited in 2.0 (after use crs())
+  QgsCoordinateReferenceSystem srsTarget = lyrTarget->srs();
+  QgsCoordinateReferenceSystem srsReference = lyrReference->srs();
 
-    mCoordTransform = new QgsCoordinateTransform(srsTarget, srsReference);
+  mCoordTransform = new QgsCoordinateTransform( srsTarget, srsReference );
 
-    mFuncTransform = ( srsTarget != srsReference)
+  mFuncTransform = ( srsTarget != srsReference )
                    ? &QgsGeometryCoordinateTransform::setGeomTransform
                    : &QgsGeometryCoordinateTransform::setNoneGeomTransform;
 
 } // void QgsGeometryCoordinateTransform::setCoordinateTransform(QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference)
 
-void QgsGeometryCoordinateTransform::transform(QgsGeometry *geom)
+void QgsGeometryCoordinateTransform::transform( QgsGeometry *geom )
 {
-    (this->*mFuncTransform)(geom);
+  ( this->*mFuncTransform )( geom );
 
 } // void QgsGeometryCoordinateTransform::transformCoordenate()
 
-void QgsGeometryCoordinateTransform::setGeomTransform(QgsGeometry *geom)
+void QgsGeometryCoordinateTransform::setGeomTransform( QgsGeometry *geom )
 {
-    geom->transform(*mCoordTransform);
+  geom->transform( *mCoordTransform );
 
 } // void QgsGeometryCoordinateTransform::setGeomTransform(QgsGeometry *geom)

src/plugins/spatialquery/qgsgeometrycoordinatetransform.h
@@ -29,44 +29,44 @@
 */
 class QgsGeometryCoordinateTransform
 {
-public:
+  public:
     /**
     * \brief Constructor for a Geometry Coordinate Transform.
     *
     */
-    QgsGeometryCoordinateTransform () {};
+    QgsGeometryCoordinateTransform() {};
 
     /**
     * \brief Destructor
     */
-    ~QgsGeometryCoordinateTransform ();
+    ~QgsGeometryCoordinateTransform();
 
     /**
     * \brief Sets the coordinate reference system the target and reference layer
     * \param lyrTarget      target layer.
     * \param lyrReference   reference layer.
     */
-    void setCoordinateTransform(QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference);
+    void setCoordinateTransform( QgsVectorLayer* lyrTarget, QgsVectorLayer* lyrReference );
 
     /**
     * \brief Transform the coordinates reference system of the geometry, if target have the different system of reference
     * \param geom      Geometry
     */
-    void transform(QgsGeometry *geom);
-private:
+    void transform( QgsGeometry *geom );
+  private:
     /**
     * \brief Transform the coordinates reference system of the geometry (use by transform)
     * \param geom      Geometry
     */
-    void setGeomTransform(QgsGeometry *geom);
+    void setGeomTransform( QgsGeometry *geom );
     /**
     * \brief None transform the coordinates reference system of the geometry (use by transform)
     * \param geom      Geometry
     */
-    void setNoneGeomTransform(QgsGeometry *geom) {};
+    void setNoneGeomTransform( QgsGeometry *geom ) {};
 
     QgsCoordinateTransform * mCoordTransform;
-    void (QgsGeometryCoordinateTransform::* mFuncTransform)(QgsGeometry *);
+    void ( QgsGeometryCoordinateTransform::* mFuncTransform )( QgsGeometry * );
 };
 
 #endif // GEOMETRYCOORDINATETRANSFORM_H

src/plugins/spatialquery/qgsmngprogressbar.cpp
@@ -19,32 +19,32 @@
 
 #include "qgsmngprogressbar.h"
 
-MngProgressBar::MngProgressBar(QProgressBar *pb)
+MngProgressBar::MngProgressBar( QProgressBar *pb )
 {
-    mPb = pb;
-    mPb->reset();
+  mPb = pb;
+  mPb->reset();
 } // MngProgressBar::MngProgressBar(QProgressBar *pb)
 
-void MngProgressBar::init(int minimum, int maximum)
+void MngProgressBar::init( int minimum, int maximum )
 {
-    mPb->reset();
-    mPb->setRange(minimum, maximum);
+  mPb->reset();
+  mPb->setRange( minimum, maximum );
 
 } // void MngProgressBar::init(int minimum, int maximum)
 
-void MngProgressBar::setFormat(QString format)
+void MngProgressBar::setFormat( QString format )
 {
 // special caracters:
 // %p - is replaced by the percentage completed.
 // %v - is replaced by the current value.
 // %m - is replaced by the total number of steps.
-    mPb->setFormat(format);
+  mPb->setFormat( format );
 
 } // void MngProgressBar::setFormat(QString format)
 
-void MngProgressBar::step(int step)
+void MngProgressBar::step( int step )
 {
-        mPb->setValue(step);
-        mPb->repaint();
+  mPb->setValue( step );
+  mPb->repaint();
 
 } // void MngProgressBar::step()

src/plugins/spatialquery/qgsmngprogressbar.h
@@ -27,39 +27,39 @@
 */
 class MngProgressBar
 {
-public:
+  public:
     /**
     * \brief Constructor for a MngProgressBar.
     * \param pb Pointer to the MngProgressBar object.
     */
-    MngProgressBar(QProgressBar *pb);
+    MngProgressBar( QProgressBar *pb );
     /**
     * \brief Destructor
     */
     ~MngProgressBar() { mPb->reset(); };
-    
+
     /**
     * \brief Sets the progress bar's minimum and maximum values to minimum and maximum respectively
     * \param minimum minimun value.
     * \param maximum maximum value.
     */
-    void init(int minimum, int maximum);
+    void init( int minimum, int maximum );
 
     /**
     * \brief Sets the format the current text.
     * \param format This property holds the string used to generate the current text.
     */
-    void setFormat(QString format);
+    void setFormat( QString format );
 
     /**
     * \brief Sets current value progress bar's
     * \param step current value
     */
-    void step(int step );
+    void step( int step );
 
-private:
+  private:
     QProgressBar * mPb;
-    
+
 };
 
 #endif // QGSMNGPROGRESSBAR_H

commit 0bc6bdd4f9c722e91888647ef06ca3b180a85b08

fix crash introduced in r13360
git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13362 c8812cc2-4d05-0410-92ff-de0c093fc19c

src/plugins/spatialquery/qgsspatialquery.cpp
@@ -150,9 +150,10 @@ short int QgsSpatialQuery::dimensionGeometry( QGis::GeometryType geomType )
       break;
     case QGis::Polygon:
       dimGeom = 2;
+      break;
     default:
       Q_ASSERT( 0 );
-      dimGeom = 0;
+      break;
   }
   return dimGeom;
 
@@ -217,7 +218,7 @@ void QgsSpatialQuery::setSpatialIndexReference()
 
 void QgsSpatialQuery::execQuery( QSet<int> & qsetIndexResult, int relation )
 {
-  // Set function GEOS
+  // Set GEOS function
   char( *operation )( const GEOSGeometry *, const GEOSGeometry* );
   switch ( relation )
   {
@@ -276,8 +277,7 @@ void QgsSpatialQuery::execQuery( QSet<int> & qsetIndexResult, int relation )
     geomTarget = featureTarget.geometry();
     coordinateTransform->transform( geomTarget );
 
-    ( this->*funcPopulateIndexResult )
-    ( qsetIndexResult, featureTarget.id(), geomTarget, operation );
+    ( this->*funcPopulateIndexResult )( qsetIndexResult, featureTarget.id(), geomTarget, operation );
   }
   delete coordinateTransform;
 